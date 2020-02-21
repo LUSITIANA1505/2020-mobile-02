8615e791afc45c445e7b06a8613000e358ef17fc
Lusi Tiana <lucytiana1504@gmail.com>
2020-02-21 11:31:52 +0700

    Market_Place

--- app/src/main/java/id/putraprima/marketplacelayout/MainActivity.java
+++ app/src/main/java/id/putraprima/marketplacelayout/MainActivity.java
@@ -2,7 +2,9 @@ package id.putraprima.marketplacelayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void postLogin (View view) {
+        Intent intent = new Intent(this, Login.class);
+        startActivity(intent);
+    }
+
 }
